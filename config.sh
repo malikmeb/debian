0ff9626bb721604b9f397471627391f8df06151f
malikmeb <56023841+malikmeb@users.noreply.github.com>
2020-09-21 09:33:44 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 Menu()
 {
@@ -19,18 +18,24 @@ sudoer()
 {
 	echo '*****Utilisateur à mettre en sudo:*****'
 	read username
-	sudo usermod -aG sudo $username
+	echo 'var_username ALL=(ALL:ALL) ALL' >> /etc/sudoers
+	sed -i -e "s/var_username/$username/g" /etc/sudoers
+	echo " "
+	echo "*********************************************"
+	echo "*************** SUDO CONFIGURÉ ***************"
+	echo "*********************************************"
 	Menu
-	read choice
+	read -p "Saisir votre choix :" choice
 }
 Cartes()
 {
 	ip a | grep ens 
 	echo '*****insérer la carte a configuré:*****'
-	read carte1
+	read -p "Saisir votre choix :" carte1
+	echo " " >> /etc/network/interfaces
 	echo auto $carte1 >> /etc/network/interfaces
 	echo '*****static ou dhcp?*****'
-	read type 
+	read -p "Saisir votre choix :" type 
 if [ $type = "static" ]
 then
 	echo iface $carte1 inet $type  >> /etc/network/interfaces
@@ -39,9 +44,19 @@ then
 	echo iface $carte1 inet $type  >> /etc/network/interfaces
 fi
 	echo "*****Adresse IP de la machine? (indiquez le CIDR)*****"
-	read ip_adress
+	read -p "Saisir votre choix :" ip_adress
 	echo address $ip_adress >> /etc/network/interfaces
 	systemctl restart networking.service
+	echo "***** Indiquez la carte connectée en NAT *****"
+	ip a
+	read -p "Saisir votre choix :" nat_card
+	sudo ifup $nat_card
+	dhclient -r
+
+	echo " "
+	echo "*********************************************"
+	echo "*************** CARTE CONFIGURÉE ***************"
+	echo "*********************************************" 
 
 echo "*****Voulez-vous configurer une autre carte?(y/n)*****"
 read choice2
@@ -52,7 +67,7 @@ then
 elif [ $choice2 = "n" ]
 then
 	Menu
-	read choice
+	read -p "Saisir votre choix :" choice
 fi
 }
 
@@ -61,26 +76,26 @@ dhcp()
 {
 	apt-get -y install isc-dhcp-server
 	rm /etc/dhcp/dhcpd.conf
-	touch dhcpd.conf
+	touch /etc/dhcp/dhcpd.conf
 	echo "*****indiquez le sous-réseau:*****"
-	read subnet
+	read -p "Saisir votre choix :" subnet
 	echo "*****indiquez le masque de sous-réseau:*****"
-	read net_mask
+	read -p "Saisir votre choix :" net_mask
 	echo "*****Indiquez le start range:*****"
-	read range_start
+	read -p "Saisir votre choix :" range_start
 	echo "*****Indiquez le end range:*****"
-	read range_end
+	read -p "Saisir votre choix :" range_end
 	echo "*****Indiquez l'adresse DNS*****"
-	read dns_ip
+	read -p "Saisir votre choix :" dns_ip
 	echo "*****Indiquez l'adresse du routeur:*****"
-	read router_ip
+	read -p "Saisir votre choix :" outer_ip
 
+	echo 'default-lease-time 86400; max-lease-time 172800;' >> /etc/dhcp/dhcpd.conf
+	echo 'subnet var_subnet netmask var_netmask { range var_range_start var_range_end; option domain-name-servers var_dns_ip; option routers var_router_ip; }' >> /etc/dhcp/dhcpd.conf
+	sed -i -e "s/var_subnet/$subnet/g" -e "s/var_netmask/$net_mask/g" /etc/dhcp/dhcpd.conf
+	sed -i -e "s/var_range_start/$range_start/g" -e "s/var_range_end/$range_end/g" /etc/dhcp/dhcpd.conf
+	sed -i -e "s/var_dns_ip/$dns_ip/g" -e "s/var_router_ip/$router_ip/g" /etc/dhcp/dhcpd.conf
 
-	sed -i -e "s/ip_address/$subnet/g" -e "s/net_mask/$net_mask/g" dhcpd.conf
-	sed -i -e "s/range_start/$range_start/g" -e "s/range_end/$range_end/g" dhcpd.conf
-	sed -i -e "s/dns_ip/$dns_ip/g" -e "s/router_ip/$router_ip/g" dhcpd.conf
-
-	mv dhcpd.conf /etc/dhcp/
 
 	echo "*****Sur quelle carte Voulez-vous que le dhcp soit connecté?*****"
 	echo " "
@@ -88,8 +103,11 @@ dhcp()
 
 	read dhcp_listen
 
-	sed -i -e 's/4=""/"$dhcp_listen"/g' /etc/default/isc-dhcp-server
 
+	sed -i -e '/INTERFACESv4/d' /etc/default/isc-dhcp-server
+	echo 'INTERFACESv4="CARTE_V4"' >> /etc/default/isc-dhcp-server
+	#echo INTERFACESv4=\"\$dhcp_listen\"\ >> /etc/default/isc-dhcp-server
+	sed -i -e "s/CARTE_V4/$dhcp_listen/g" /etc/default/isc-dhcp-server
 	systemctl restart isc-dhcp-server
 
 	echo "*********************************************"
@@ -97,7 +115,7 @@ dhcp()
 	echo "*********************************************"
 
 	Menu
-	read choice
+	read -p "Saisir votre choix :" choice
 
 }
 
@@ -105,19 +123,21 @@ dns()
 {
 	apt-get install -y bind9 bind9utils
 	echo "*****Nom de domaine?:*****"
-	read dns_name
-	echo "zone $dns_name { type master; file '/etc/bind/db.$dns_name'; };" >> /etc/bind/named.conf.local
+	read -p "Saisir votre choix :" dns_name
+	echo 'zone var_dns_name { type master; file "/etc/bind/db.var_dns_name"; };' >> /etc/bind/named.conf.local
+	sed -i -e "s/var_dns_name/$dns_name/g" /etc/bind/named.conf.local
 	cp /etc/bind/db.empty /etc/bind/db.$dns_name
-	sed -i -e 's/localhost./$dns_name./g' /etc/bind/db.$dns_name
-	sed -i -e 's/root.localhost./root.$dns_name./g' /etc/bind/db.$dns_name
-	sed -i -e 's/@	IN 	NS 	$dns_name./" "/g' /etc/bind/db.$dns_name
-	echo "$dns_name. IN 	NS 	ns.$dns_name." >> /etc/bind/db.$dns_name
+	sed -i -e "s/root.localhost./root.$dns_name./g" /etc/bind/db.$dns_name
+	sed -i -e "s/localhost./ns.$dns_name./g" /etc/bind/db.$dns_name
+	sed -i '$d' /etc/bind/db.$dns_name
+
+	echo 'var_dns_name.  IN  NS  ns.var_dns_name.' >> /etc/bind/db.$dns_name
+	sed -i -e "s/var_dns_name/$dns_name/g" /etc/bind/db.$dns_name
 	echo "*****Adresse IP du DNS hôte?:*****"
-	read dns_ip_hote
-	echo "ns	 IN 	A 	$dns_ip_hote" >> /etc/bind/db.$dns_name
+	read -p "Saisir votre choix :" dns_ip_hote
+	echo "ns   IN      A      $dns_ip_hote" >> /etc/bind/db.$dns_name
 	echo 'options { directory "/var/cache/bind";' > /etc/bind/named.conf.options
 	echo ' ' >> /etc/bind/named.conf.options
-	echo 'multiple' >> etc/bind/named.conf.options
 	echo 'allow-recursion { any; }; forwarders { 8.8.8.8; }; dnssec-enable no; dnssec-validation no; auth-nxdomain no; listen-on-v6 { any; }; listen-on { any; }; allow-query { any; }; };' >> /etc/bind/named.conf.options
 	sudo named-checkconf
 	sudo named-checkzone $dns_name /etc/bind/db.$dns_name
@@ -128,32 +148,37 @@ dns()
 	echo "*********************************************"
 
 	Menu 
-	read choice
+	read -p "Saisir votre choix :" choice
 
 }
 
 enregistrement()
 {
+	echo "***** Nom de domaine?:*****"
+	read -p "Saisir votre choix :" dns_name
 	echo "*****FQDN?:*****"
-	read fqdn
+	read -p "Saisir votre choix :" fqdn
 	echo "*****Type d'enregistrement (A/CNAME/MX/TXT)*****"
-	read type_enregistrement
+	read -p "Saisir votre choix :" type_enregistrement
 	echo "*****Adresse IP*****"
-	read ip_enregistrement
+	read -p "Saisir votre choix :" ip_enregistrement
 
-	echo "$fqdn	 IN 	$type_enregistrement 	$ip_enregistrement" >> /etc/bind/db.$dns_name
-
-	echo "***** enregistrement enregistré*****"
+	echo 'var_fqdn	 IN 	var_type_enregistrement 	var_ip_enregistrement' >> /etc/bind/db.$dns_name
+	sed -i -e "s/var_fqdn/$fqdn/g" -i -e "s/var_type_enregistrement/$type_enregistrement/g" -i -e "s/var_ip_enregistrement/$ip_enregistrement/g" /etc/bind/db.$dns_name
+	echo " "
+	echo "*********************************************"
+	echo "********enregistrement enregistré************"
+	echo "*********************************************"
 	echo " "
 	echo "*****Voulez-vous ajouter un autre enregistrement? (y/n) *****"
-	read retry_enregistrement
+	read -p "Saisir votre choix :" retry_enregistrement
 	if [ retry_enregistrement = 'y' ]
 	then
 		enregistrement
 	elif [ retry_enregistrement = 'n' ]
 	then 
 		Menu
-		read choice
+		read -p "Saisir votre choix :" choice
 	fi
 }
 
@@ -168,32 +193,38 @@ sup_enregitrement()
 
 	if [ $choice_sup = '1' ]
 	then
+		echo "***** Nom de domaine?:*****"
+		read -p "Saisir votre choix :" dns_name
 		echo "***** Indiquez l'adresse IP *****"
-		read sup_ip
-		sed -i -e 's/$fqdn	 IN 	$type_enregistrement 	$sup_ip/" "/g' /etc/bind/db.$dns_name
+		read -p "Saisir votre choix :" sup_ip
+		sed -i "/$sup_ip/d" /etc/bind/db.$dns_name
 		echo "***** DONE *****"
 		Menu
-		read choice
+		read -p "Saisir votre choix :" choice
 	elif [ $choice_sup = 2 ]
-	then 
+	then
+		echo "***** Nom de domaine?:*****"
+		read -p "Saisir votre choix :" dns_name
 		echo "***** Indiquez le FQDN *****"
-		read sup_fqdn
-		sed -i -e 's/$sup_fqdn	 IN 	$type_enregistrement 	$ip_enregistrement/" "/g' /etc/bind/db.$dns_name
+		read -p "Saisir votre choix :" sup_fqdn
+		sed -i "/$sup_fqdn/d" /etc/bind/db.$dns_name
 		echo "***** DONE *****"
 		Menu
-		read choice
+		read -p "Saisir votre choix :" choice
 	elif [ $choice_sup = 3 ]
 	then
+		echo "***** Nom de domaine?:*****"
+		read -p "Saisir votre choix :" dns_name
 		echo "***** Indiquez le type d'enregistrement *****"
-		read sup_enregistrement
-		sed -i -e 's/$fqdn	 IN 	$sup_enregistrement 	$ip_enregistrement/" "/g' /etc/bind/db.$dns_name
+		read -p "Saisir votre choix :" sup_enregistrement
+		sed -i "/$sup_enregistrement/d" /etc/bind/db.$dns_name
 		echo "***** DONE *****"
 		Menu
-		read choice
+		read -p "Saisir votre choix :" choice
 	elif [ $choice_sup = 4 ]
 	then
 		Menu 
-		read choice
+		read -p "Saisir votre choix :" choice
 	fi
 }
 
@@ -202,7 +233,7 @@ reset()
 	echo "********1. BACK-UP DHCP*************"
 	echo "********2. BACK-UP DNS**************"
 
-	read choice_reset
+	read -p "Saisir votre choix :" choice_reset
 
 	if [ $choice_reset = 1 ]
 	then
@@ -214,7 +245,7 @@ reset()
 	fi
 
 	Menu
-	read choice
+	read -p "Saisir votre choix :" choice
 }
 
 
@@ -223,7 +254,7 @@ reset()
 
 
 Menu
-read choice
+read -p "Saisir votre choix :" choice
 #-----------------sudo user------------------------#
 if [ $choice = 1 ]
 then
